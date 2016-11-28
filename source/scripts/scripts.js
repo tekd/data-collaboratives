3551375167d67b1ba34a5160964c9ca2327d298a
Ena Bek <enabek@gmail.com>
2016-11-28 17:39:33 -0500

    Add valueNames to table cells and options hash, cannot instantiate new List

--- source/scripts/scripts.js
+++ source/scripts/scripts.js
@@ -7,44 +7,32 @@ $(document).ready(function() {
           multiSearch: true
       };
 
-  // var options = {
-  //   valueNames: [ {name:'company__name', attr:'data-target'}, 'company__category', 'company__type', 'company__founded', {name:'company__location', attr:'data-target'}, 'company__last-update', { attr: 'data-location', name: 'filter' }, {name: 'company__other-sources', attr: 'data-target'}, {name:'company__data-sectors',attr:'data-target'}, {name:'company__federal-sources', attr: 'data-target'}, {name:'company__provincial-sources', attr: 'data-target'} ],
-  //       plugins: [ ListFuzzySearch() ]
-  // };
-  //
-  // var cases = new List('case_data', options);
+  var options = {
+        valueNames: [ {attr: 'case-name', name: 'case'}, {attr: 'case-region', name: 'region'}, {attr: 'case-sector', name: 'sector'}, {attr: 'case-type', name: 'type'} ]
+        ,
+        plugins: [ ListFuzzySearch() ]
+    };
 
+  // var caseList = new List('case_data', options);
 
 
   var searchButtons = $('.table-sortable__search').find("button[type='submit']")
 
-    searchButtons.on("click", function(e) {
-        e.preventDefault();
-        if ($(this).parent().hasClass("table-sortable__search--active")) {
-            $(this).parent().removeClass("table-sortable__search--active");
-            searchReset();
-        }
-    });
+  searchButtons.on("click", function(e) {
+      e.preventDefault();
+      if ($(this).parent().hasClass("table-sortable__search--active")) {
+          $(this).parent().removeClass("table-sortable__search--active");
+          searchReset();
+      }
+  });
 
 
   // SORT ICON
-      var sortClickButtons = $(".table-sortable__control > i:contains('keyboard_arrow_down')");
-      sortClickButtons.on("click", function() {
-          $(this).text() == "keyboard_arrow_down" ? $(this).text("keyboard_arrow_up") : $(this).text("keyboard_arrow_down");
-
-      });
-
+  var sortClickButtons = $(".table-sortable__control > i:contains('keyboard_arrow_down')");
+  sortClickButtons.on("click", function() {
+      $(this).text() == "keyboard_arrow_down" ? $(this).text("keyboard_arrow_up") : $(this).text("keyboard_arrow_down");
+  });
 
 
-  // CLEAR ALL FILTERS
-    // $(".clear_filters").on("click", function() {
-    //     allFilters.each(function(idx,filter) {
-    //         $('#'+filter.id).prop('selectedIndex',0);
-    //     });
-    //     $(".circle.active").attr("class","circle");
-    //     companyList.filter();
-    //     searchReset();
-    //     companyList.sort('company__name', { order: "asc" });
-    // });
 
 });
