0ffc4a7da234c46b9910ed81e140a15708d81732
Denny Tek <tek.denny@gmail.com>
2017-01-07 20:43:57 -0500

    Add deck with horizontal scroll

---
+++ source/js/introduction.js
@@ -0,0 +1,74 @@
+$(document).ready(function() {
+  // FULLPAGE
+  $('#fullpage').fullpage({
+    //Navigation
+    menu: '#menu',
+    lockAnchors: false,
+    anchors: ["section1","section2","section3","section4","section5","section6", "learn-more"],
+    navigation: true,
+    navigationPosition: 'right',
+    navigationTooltips: ["Section 1","Section 2","Section 3","Section 4","Section 5","Section 6", "Learn More"],
+    showActiveTooltip: false,
+    slidesNavigation: false,
+    slidesNavPosition: 'bottom',
+
+    //Scrolling
+    css3: true,
+    scrollingSpeed: 1000,
+    autoScrolling: true,
+    fitToSection: true,
+    fitToSectionDelay: 1000,
+    scrollBar: true,
+    easing: 'easeInOutCubic',
+    easingcss3: 'ease',
+    loopBottom: false,
+    loopTop: false,
+    loopHorizontal: true,
+    continuousVertical: false,
+    continuousHorizontal: true,
+    scrollHorizontally: false,
+    interlockedSlides: false,
+    dragAndMove: false,
+    offsetSections: false,
+    resetSliders: false,
+    fadingEffect: false,
+    normalScrollElements: '#element1, .element2',
+    scrollOverflow: false,
+    scrollOverflowReset: false,
+    scrollOverflowOptions: null,
+    touchSensitivity: 15,
+    normalScrollElementTouchThreshold: 5,
+    bigSectionsDestination: null,
+
+    //Accessibility
+    keyboardScrolling: true,
+    animateAnchor: true,
+    recordHistory: true,
+
+    //Design
+    controlArrows: true,
+    verticalCentered: true,
+    // sectionsColor : ['#ccc', '#fff'],
+    // paddingTop: '3em',
+    // paddingBottom: '10px',
+    // fixedElements: '.top-bar',
+    responsiveWidth: 0,
+    responsiveHeight: 0,
+    responsiveSlides: false,
+
+    //Custom selectors
+    sectionSelector: '.section',
+    slideSelector: '.slide',
+
+    lazyLoading: true,
+
+    //events
+    onLeave: function(index, nextIndex, direction){},
+    afterLoad: function(anchorLink, index){},
+    afterRender: function(){},
+    afterResize: function(){},
+    afterResponsive: function(isResponsive){},
+    afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+    onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
+  }); //END FULL PAGE
+});
